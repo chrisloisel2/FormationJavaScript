0c64804d3088e17531d79b9235043dc4a12ed50c
Christopher Loisel <christopher@MacBook-Pro-de-Christopher.local>
2023-12-07 14:01:24 +0100

    Classes

--- index.js
+++ index.js
@@ -21,3 +21,30 @@ tab.forEach((element) => {
 //index0, 1, 2, 3, 4
 
 console.log(tri(2, 3, (a, b) => a > b)); // 3
+
+// Creation de ma classe
+
+class Voiture {
+    roues = 4;
+    couleur = "rouge";
+    porte = 5;
+    marque = "Renault";
+
+    constructor(roues, couleur, porte, marque) {
+        this.roues = roues;
+        this.couleur = couleur;
+        this.porte = porte;
+        this.marque = marque;
+        console.log("Je suis une voiture");
+    }
+
+    avancer() {
+        console.log("vroum vroum");
+    }
+}
+
+// Creation d'un objet
+let vroum = new Voiture(4, "grise", 4, "lambo");
+
+console.log(vroum);
+vroum.avancer();
