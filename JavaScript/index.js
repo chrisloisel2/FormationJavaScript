633c7ce4161ca301da8e954bb73ed64949e40934
Christopher Loisel <christopher@MacBook-Pro-de-Christopher.local>
2023-12-08 16:19:11 +0100

    tp fetch

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -22,11 +22,46 @@ function add(a) {
 
 let lst = [2, 4, 6, 8, 10, 12];
 
-lst[i];
-
 // Fonction de premier ordre
 
 lst.forEach(add);
 lst.forEach((a) => {
     console.log(a + 6);
 });
+
+// fetch
+// fetch envoyer une requete http au backend, attendre la reponse,
+// des que la reponse arrive, on va executer le code qui est dans le then
+fetch("https://api.chucknorris.io/jokes/random").then((reponse) => {
+    // Recu un objet reponse au format json
+    reponse.json().then((data) => {
+        console.log(data);
+    });
+});
+
+// fetch
+// fetch envoyer une requete http au backend, attendre la reponse,
+// des que la reponse arrive, on va executer le code qui est dans le then
+fetch("https://api.chucknorris.io/jokes/random").then(deJson);
+
+function deJson(reponse) {
+    reponse.json().then(print);
+}
+
+function print(a) {
+    console.log(a);
+}
+
+// Faire un appel api sur cette adresse et affiche les retours dans la console
+
+// http://82.165.127.44/api/users
+
+// Faire un appel api sur cette adresse et recuperer uniquement l'utilisateur demander
+
+// http://82.165.127.44/api/users/:id
+
+// Vous viendrez ensuite ajouter des utilisateurs avec un post sur cette adresse
+
+// http://82.165.127.44/api/users
+
+// async
